6cec8c822e380c3fa727f4578221c0d2a2bd15e2
leozx01 <114406354+leozx01@users.noreply.github.com>
2022-09-26 20:35:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wx55f82aaab9040e02',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '190408a9fddef9fa7d78794954ae78eb',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oMMjp6Vati0LS5g-BSoDe7E-Qth0',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'u4HJKMEJswdrtB6KxO9KMPsicx28svqRCvS1DH4Bscs',
       // 所在省份
       province: '广东',
       // 所在城市
