934c590eea7d5803fcf61fcc21325844af84913f
leozx01 <114406354+leozx01@users.noreply.github.com>
2022-09-26 21:04:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -264,19 +264,19 @@ const USER_CONFIG = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {
-      type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
-    },
-    {
-      type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
-    },
-    // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {
-      type: '生日', name: '李四', year: '1996', date: '09-31', isShowAge: true,
-    },
-    {
-      type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
-    }
+//     {
+//       type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
+//     },
+//     {
+//       type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+//     },
+//     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//     {
+//       type: '生日', name: '李四', year: '1996', date: '09-31', isShowAge: true,
+//     },
+//     {
+//       type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+//     }
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -296,11 +296,11 @@ const USER_CONFIG = {
    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    { keyword: 'love_day', date: '2022-09-08' },
-    // 结婚纪念日
-    { keyword: 'marry_day', date: '2022-09-09' },
-    // 退伍日
-    { keyword: 'ex_day', date: '2022-09-10' }
+//     { keyword: 'love_day', date: '2022-09-08' },
+//     // 结婚纪念日
+//     { keyword: 'marry_day', date: '2022-09-09' },
+//     // 退伍日
+//     { keyword: 'ex_day', date: '2022-09-10' }
     // 你可以不断按格式往下增加
     // ...
   ],
