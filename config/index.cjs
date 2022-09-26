ab496a21522ddde7dbaebfb9d129dd869ce3a333
leozx01 <114406354+leozx01@users.noreply.github.com>
2022-09-26 20:58:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -65,44 +65,44 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oMMjp6Vati0LS5g-BSoDe7E-Qth0',
+      id: 'oMMjp6eJi9MejUINguzWXqxwBzJc',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'u4HJKMEJswdrtB6KxO9KMPsicx28svqRCvS1DH4Bscs',
       // 所在省份
       province: '广东',
       // 所在城市
-      city: '肇庆',
+      city: '昆明',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-07',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://wangxinleo.cn',
+      openUrl: '#',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
-        },
-        {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
-        },
+                  {
+                    type: '*生日', name: '宝贝老婆', year: '2001', date: '08-07',
+                  },
+                  {
+                    type: '节日', name: '我们见面', year: '2023', date: '01-05',
+                  },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
-        }
+        //         {
+        //           type: '生日', name: '李四', year: '1996', date: '09-31',
+        //         },
+        //         {
+        //           type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+        //         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-08-26' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+//         { keyword: 'marry_day', date: '2022-09-09' },
+//          退伍日
+//         { keyword: 'ex_day', date: '2022-09-10' }
       ],
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
@@ -226,7 +226,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oMMjp6Vati0LS5g-BSoDe7E-Qth0',
     }
     // 你可以不断按格式往下增加
     // ...
